d1d5346e64e63c3210d4b88ad45fdc4780128493
MO EYMEN KARAM <mohamadkaram839@gmail.com>
2022-05-29 14:17:53 +0300

    29.05:02

--- src/blog/views.py
+++ src/blog/views.py
@@ -3,22 +3,22 @@ from matplotlib.style import context
 
 posts=[
     {'title':'Birinci paylaşım',
-    'content':'Birinici paylaşım içeriği',
+    'content':'Birinici paylaşım içeriği .. Birinici paylaşım içeriği .. Birinici paylaşım içeriği .. Birinici paylaşım içeriği .. Birinici paylaşım içeriği .. Birinici paylaşım içeriği .. Birinici paylaşım içeriği .. Birinici paylaşım içeriği .. Birinici paylaşım içeriği .. Birinici paylaşım içeriği .. ',
     'post_date':'15-1-2019',
     'author':'ahmed Bey'
     },
     {'title':'İkinci paylaşım',
-    'content':'Birinici paylaşım içeriği',
+    'content':'İkinci paylaşım içeriği .. İkinci paylaşım içeriği .. İkinci paylaşım içeriği .. İkinci paylaşım içeriği .. İkinci paylaşım içeriği .. İkinci paylaşım içeriği .. İkinci paylaşım içeriği .. İkinci paylaşım içeriği .. İkinci paylaşım içeriği .. İkinci paylaşım içeriği .. ',
     'post_date':'15-2-2019',
     'author':'Efe Bey'
     },
     {'title':'Üçüncü paylaşım',
-    'content':'Birinici paylaşım içeriği',
+    'content':'üçünü paylaşım içeriği .. üçünü paylaşım içeriği .. üçünü paylaşım içeriği .. üçünü paylaşım içeriği .. üçünü paylaşım içeriği .. üçünü paylaşım içeriği .. üçünü paylaşım içeriği .. üçünü paylaşım içeriği .. üçünü paylaşım içeriği .. üçünü paylaşım içeriği ..',
     'post_date':'15-3-2019',
     'author':'Ali Bey'
     },
     {'title':'Dörtüncü paylaşım',
-    'content':'ikinci paylaşım içeriği',
+    'content':'Dörtüncü paylaşım içeriği ..  Dörtüncü paylaşım içeriği .. Dörtüncü paylaşım içeriği .. Dörtüncü paylaşım içeriği .. Dörtüncü paylaşım içeriği .. Dörtüncü paylaşım içeriği .. Dörtüncü paylaşım içeriği .. Dörtüncü paylaşım içeriği .. Dörtüncü paylaşım içeriği .. Dörtüncü paylaşım içeriği .. ',
     'post_date':'15-4-2019',
     'author':'Mehmet Bey'
     }
@@ -31,5 +31,9 @@ def home(request):
         'posts':posts,
     }
     return render(request, 'blog/index.html' , context)
+
+def about(request):
+    
+    return render(request, 'blog/about.html' )
 
 
